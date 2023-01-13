a405c4a4b04dab23e910310264e885f1b186a807
vince <vincent.harpin@orange.fr>
2023-01-13 10:06:28 +0100

    exercice 145

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);
+echo $fruits[1];
 
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age["Ben"] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y;
 }
 
 
@@ -21,14 +22,19 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
 
+sort($colors);
+print_r($colors);
+
+rsort($colors);
+print_r($colors);
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
 
+sort($age);
+print_r($age);
 
 
 
